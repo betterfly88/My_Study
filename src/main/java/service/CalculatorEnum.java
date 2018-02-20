2d1e904c8ebac05202c7d320389cac3977564b21
betterfly <betterfly4202@gmail.com>
2018-02-21 00:49:40 +0900

    감이 안잡힌다아아아ㅏㅏㅏ

--- src/main/java/service/CalculatorEnum.java
+++ src/main/java/service/CalculatorEnum.java
@@ -1,5 +1,8 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by betterFLY on 2018. 2. 19.
  * Github : http://github.com/betterfly88
@@ -7,7 +10,7 @@ package service;
 
 public class CalculatorEnum {
     enum Calculation{
-        PLUS("\\+"){
+        PLUS("+"){
             @Override
             public double Calculator(double a, double b){
                 return a + b;
@@ -47,22 +50,30 @@ public class CalculatorEnum {
 
 
     public static void main(String [] args){
-        String a = "2 + 3";
+        String a = "2  +   3";
         a = a.replaceAll(" ","");
-        String [] dd = a.split(Calculation.PLUS.getValue());
+//        String [] dd = a.split(Calculation.PLUS.getValue());
 
-//        for(int i=0; i<dd.length; i++){
-//            int temp = Integer.parseInt(dd[i]);
-//            System.out.println(temp);
-//        }
+        List stringList = new ArrayList<String>();
 
-        for(String s : dd){
-//            System.out.println(s);
-            s.replaceAll(" ","");
-            int temp = Integer.parseInt(s);
-            System.out.println(temp);
+        for(int i=0 ; i<a.length(); i++){
+//            System.out.print(a.charAt(i));
+            final String getNum = new String(new char[] { a.charAt(i) }, 0, 1);
 
+            if(getNum.equals(Calculation.PLUS.getValue())){
+                getNum.split(Calculation.PLUS.value);
+                stringList.add(getNum);
+                System.out.println("!?"+stringList);
+            }
         }
 
+
+//        for(String s : dd){
+////            System.out.println(s);
+//            s.replaceAll(" ","");
+//            int temp = Integer.parseInt(s);
+//            System.out.println(temp);
+//        }
     }
+
 }
