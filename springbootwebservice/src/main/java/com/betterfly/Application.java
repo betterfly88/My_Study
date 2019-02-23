af124b7238eb20e120b0d9bbad6707e7c7818aa7
이충일 <betterfly4202@gmail.com>
2019-02-24 03:49:19 +0900

    spinrg boot getting started

---
+++ springbootwebservice/src/main/java/com/betterfly/Application.java
@@ -0,0 +1,16 @@
+package com.betterfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by betterfly
+ * Date : 2019.02.24
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ springbootwebservice/src/test/java/test/betterfly/ApplicationTest.java
@@ -0,0 +1,8 @@
+package test.betterfly;
+
+/**
+ * Created by betterfly
+ * Date : 2019.02.24
+ */
+public class ApplicationTest {
+}
