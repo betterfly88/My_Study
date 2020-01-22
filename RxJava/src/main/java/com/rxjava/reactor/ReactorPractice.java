3de651cc6b3e56849c737303b003173207893391
betterfly <betterfly4202@gmail.com>
2020-01-22 18:40:04 +0900

    reactor start

--- RxJava/src/main/java/com/rxjava/reactor/ReactorPractice.java
+++ RxJava/src/main/java/com/rxjava/reactor/ReactorPractice.java
@@ -11,16 +11,16 @@ import java.util.Map;
 public class ReactorPractice {
 
     public static void main(String[] args) {
-        final List<String> fruitsBasket = Arrays.asList(new String []{"Apple", "Melon", "Kiwi", "Lemon"});
-        final List<String> fruitsBasket2 = Arrays.asList(new String []{"Banana", "Water Melon", "Grape","Apple"});
-        final List<String> fruitsBasket3 = Arrays.asList(new String []{"Lemon", "Strawberry", "Orange", "Kiwi", "Banana"});
+        final List<String> fruitsBasket = Arrays.asList(new String []{"Apple", "Melon", "Kiwi", "Lemon", "Apple", "Melon"});
+        final List<String> fruitsBasket2 = Arrays.asList(new String []{"Banana", "Water Melon", "Grape",  "Water Melon", "Grape","Apple"});
+        final List<String> fruitsBasket3 = Arrays.asList(new String []{"Lemon", "Strawberry", "Orange", "Kiwi", "Banana", "Orange", "Kiwi", "Banana", "Kiwi", "Banana"});
 
         final List<List<String>> baskets = Arrays.asList(fruitsBasket, fruitsBasket2, fruitsBasket3);
 
         final Flux<List<String>> basketFlux = Flux.fromIterable(baskets);
 
         basketFlux.concatMap(basket -> {
-            final Mono<List<String>> basketDistinctList = Flux.fromIterable(basket).distinct().collectList();
+            final Mono<List<String>> distinctFruits = Flux.fromIterable(basket).distinct().collectList();
             final Mono<Map<String, Long>> countFruitsMono = Flux.fromIterable(basket)
                     .groupBy(fruits -> fruits)
                     .concatMap(groupedFlux -> groupedFlux.count()
@@ -36,6 +36,8 @@ public class ReactorPractice {
                             putAll(currentMap);
                         }
                     });
-        });
+            return Flux.zip(distinctFruits, countFruitsMono, (distinct, count) -> new FruitInfo(distinct, count));
+
+        }).subscribe(System.out::println);
     }
 }
