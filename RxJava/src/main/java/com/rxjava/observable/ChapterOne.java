a9349fb31a4f151accedc62eec43487e44dff35e
betterfly <betterfly4202@gmail.com>
2020-01-29 22:16:12 +0900

    rxJava in chapterOne

--- RxJava/src/main/java/com/rxjava/observable/ChapterOne.java
+++ RxJava/src/main/java/com/rxjava/observable/ChapterOne.java
@@ -1,7 +1,12 @@
 package com.rxjava.observable;
 
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
+import java.util.function.Function;
 
 public class ChapterOne {
     public CompletableFuture<String> combineFuture(){
@@ -13,4 +18,36 @@ public class ChapterOne {
         });
     }
 
+    public Observable<String> combineObservable(){
+        Observable<String> o1 = Observable.just("A");
+        Observable<String> o2 = Observable.just("B");
+
+        return Observable.zip(o1, o2, (x,y) ->{
+            return x+y;
+        });
+    }
+
+    public Single<String> getDataA(){
+        return Single.<String> create(o ->{
+            o.onSuccess("Data A");
+        }).subscribeOn(Schedulers.io());
+    }
+
+    public Single<String> getDataB(){
+        return Single.just("Data B")
+                .subscribeOn(Schedulers.io());
+    }
+
+    public Completable writeToDatabase(Object data){
+        return Completable.create(s ->{
+//            doAsyncWrite(data, () -> s.onComplete(), error -> s.onError(new IllegalArgumentException()))
+            if(data.equals(1)){
+                System.out.println("성공");
+                s.onComplete();
+            }else{
+                System.out.println("실패");
+                s.onError(new IllegalArgumentException());
+            }
+        });
+    }
 }
--- RxJava/src/test/java/com/rxjava/observable/ChapterOneTest.java
+++ RxJava/src/test/java/com/rxjava/observable/ChapterOneTest.java
@@ -1,5 +1,7 @@
 package com.rxjava.observable;
 
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,7 +23,32 @@ public class ChapterOneTest {
     public void combineFuture() throws ExecutionException, InterruptedException {
 
         assertThat(target.combineFuture().get(), is("AB"));
+    }
+
+
+    @Test
+    public void combineObservable(){
+        assertThat(target.combineObservable().blockingSingle(), is("AB"));
+    }
+
+    @Test
+    public void Single_장점(){
+        System.out.println(target.getDataA().blockingGet());
+        System.out.println(target.getDataB().blockingGet());
+
+        // Observable 스트림으로 병합
+        Flowable<String> a_merge_b = target.getDataA().mergeWith(target.getDataB());
+
+        // 출력 순서는 A,B 순서를 보장하지 않는다. (각각의 처리 순서대로)
+        a_merge_b.blockingSubscribe(v ->{
+            System.out.println(v);
+        });
+
 
     }
 
+    @Test
+    public void 반환값이필요없는_Completable(){
+        target.writeToDatabase(1).blockingGet();
+    }
 }
