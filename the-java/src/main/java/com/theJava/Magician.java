0341486ae11b25984958d8bd5bb1cc9f319c6f3a
betterfly <betterfly4202@gmail.com>
2020-03-17 17:03:21 +0900

    make javaagent

--- the-java/src/main/java/com/theJava/Magician.java
+++ the-java/src/main/java/com/theJava/Magician.java
@@ -1,14 +1,6 @@
 package com.theJava;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.dynamic.ClassFileLocator;
-import net.bytebuddy.implementation.FixedValue;
-import net.bytebuddy.pool.TypePool;
-
-import java.io.File;
 import java.io.IOException;
-
-import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Magician {
     public static void main(String[] args) throws IOException {
