185aa1cb378036e0bc440e4dd15d535d3d0a5c65
betterfly <betterfly4202@gmail.com>
2020-09-02 02:35:33 +0900

    cglib

--- spriing-proxy-jpa/src/test/java/com/betterfly/proxy/RealBookServiceTest.java
+++ spriing-proxy-jpa/src/test/java/com/betterfly/proxy/RealBookServiceTest.java
@@ -1,11 +1,18 @@
 package com.betterfly.proxy;
 
+import net.sf.cglib.proxy.Callback;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -19,14 +26,46 @@ public class RealBookServiceTest {
     @Autowired
     BookService bookServiceProxy;
 
+    BookService dynamicBookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class}, new InvocationHandler() {
+        BookService bookService = new RealBookService();
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getName().equals("rent")){
+                System.out.println("before dynamicProxy");
+                Object invoke = method.invoke(bookService, args);
+
+                System.out.println("after dynamicProxy");
+                return invoke;
+            }
+
+            return  method.invoke(bookService, args);
+        }
+    });
+
     @Test
     public void 책을_렌트한다(){
+        //given
+        MethodInterceptor handler = new MethodInterceptor() {
+            BookService bookService = new RealBookService();
+
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                System.out.println("before dynamicProxy");
+                Object invoke = method.invoke(bookService, objects);
+
+                System.out.println("after dynamicProxy");
+                return invoke;
+            }
+        };
+
         //when
+        RealBookService cglibProxyService = (RealBookService) Enhancer.create(RealBookService.class, handler);
         Book b = new Book();
         b.setTitle("Proxy in Spring");
 
         //then
-        realBookService.rent(b);
+        cglibProxyService.rent(b);
     }
 
     @Test
@@ -36,5 +75,14 @@ public class RealBookServiceTest {
 
         // then
         assertTrue(books.size() == 10);
+    }
+
+    @Test
+    public void 다이나믹_프록시(){
+        Book b = new Book();
+        b.setTitle("다이나믹 프록시 교재");
+
+        dynamicBookService.rent(b);
+        dynamicBookService.findByBooks();
     }
 }
