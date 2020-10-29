df4b6a47152632071a3192087956caf2f19a77b1
betterfly <betterfly4202@gmail.com>
2020-10-29 19:48:42 +0900

    no message

--- object-mapping/src/main/java/com/betterfly/objectmapping/Starter.java
+++ object-mapping/src/main/java/com/betterfly/objectmapping/Starter.java
@@ -11,15 +11,15 @@ import java.util.List;
 public class Starter {
 
     public static void main(String[] args) {
-        List<OrderDto> orderDtoList = OrderDto.getOrderDtoList(1_000_000);
+        List<OrderDto> orderDtoList = OrderDto.getOrderDtoList(1_300_000);
 
-        AbstractMappingService mapStructService = new MapStructServiceImpl();
-        AbstractMappingService orikaService = new OrikaServiceImpl();
+//        AbstractMappingService mapStructService = new MapStructServiceImpl();
+//        AbstractMappingService orikaService = new OrikaServiceImpl();
         AbstractMappingService mapperService = new ModelMapperServiceImpl();
         for (int i = 0; i <10 ; i++) {
-            mapStructService.execute(orderDtoList);
+//            mapStructService.execute(orderDtoList);
 //            orikaService.execute(orderDtoList);
-//            mapperService.execute(orderDtoList);
+            mapperService.execute(orderDtoList);
         }
     }
 }
