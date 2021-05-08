88ec67279caf492b626564fa44f5c589862ef91c
betterfly <betterfly4202@gmail.com>
2021-05-08 23:46:43 +0900

    initilize

---
+++ spring-singleton/src/main/java/com/betterfly/springsingleton/model/Student.java
@@ -0,0 +1,4 @@
+package com.betterfly.springsingleton.model;
+
+public class Student {
+}
---
+++ spring-singleton/src/test/java/com/betterfly/springsingleton/SpringSingletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterfly.springsingleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSingletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
